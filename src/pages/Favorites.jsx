093bc0e0a93e498f2d0a1852f5019bf92877ece7
Helton Pereira <helton.bfsm@gmail.com>
2022-08-01 15:55:54 -0300

    Requisito 1 cumprido

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Favorites extends Component {
+  render() {
+    return (
+      <div>
+        <p data-testid="page-favorites">Favoritos</p>
+      </div>
+    );
+  }
+}
+
+export default Favorites;
---
+++ src/pages/ProfileEdit.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProfileEdit extends Component {
+  render() {
+    return (
+      <div data-testid="page-profile-edit">
+        <p>Editar Perfil</p>
+      </div>
+    );
+  }
+}
+
+export default ProfileEdit;
