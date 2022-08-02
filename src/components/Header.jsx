bea0db4f1cd44dcdc5b64f6ebdf664764b5ca735
Helton Pereira <helton.bfsm@gmail.com>
2022-08-02 15:17:46 -0300

    Requisito 3 cumprido

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,35 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-// import { getUser } from '../services/userAPI';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Header extends Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     name: '',
-  //     isLoading: false,
-  //   };
-  // }
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      isLoading: false,
+    };
+  }
 
-  // handleName = async () => {
-  //   const { name } = this.state;
-  //   const userName = await getUser();
-  //   this.setState({ name: userName });
-  // }
+  componentDidMount() {
+    this.handleName();
+  }
+
+  handleName = async () => {
+    this.setState({ isLoading: true });
+    const userName = await getUser();
+    this.setState({ name: userName });
+    this.setState({ isLoading: false });
+  }
 
   render() {
+    const { name, isLoading } = this.state;
+    if (isLoading) return <Loading />;
     return (
       <header data-testid="header-component">
         Trybe Tubos
-        <p data-testid="header-user-name" />
+        <p data-testid="header-user-name">{ name.name }</p>
         <Link to="/search" data-testid="link-to-search">Pesquisa</Link>
         <Link to="/favorites" data-testid="link-to-favorites">Favoritas</Link>
         <Link to="/profile" data-testid="link-to-profile">Perfil</Link>
